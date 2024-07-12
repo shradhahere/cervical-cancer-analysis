6f7e4656e0023915866e931b3d3325f4520889d5
Shradha Awasthi <43327958+shradhahere@users.noreply.github.com>
2024-07-12 01:16:32 +0100

    Update start_jupyter.sh

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -1,3 +1,3 @@
+
 #!/bin/bash
-cd /home/ec2-user/cervical-cancer-analysis-main
-nohup jupyter notebook --ip=0.0.0.0 --no-browser --port=8888 > jupyter.log 2>&1 &
+nohup jupyter notebook --notebook-dir=/home/ec2-user/cervical-cancer-analysis-main --ip=0.0.0.0 --port=8888 --no-browser &
